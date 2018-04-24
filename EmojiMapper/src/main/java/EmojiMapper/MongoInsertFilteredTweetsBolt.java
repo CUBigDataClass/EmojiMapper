439f30778d876fd8121f2d79b11d10c3000f5cbf
Harshini <harshinimkrishnan@gmail.com>
2018-04-24 06:33:09 -0600

    Insert filtered tweets into Mongodb collection

---
+++ EmojiMapper/src/main/java/EmojiMapper/MongoInsertFilteredTweetsBolt.java
@@ -0,0 +1,29 @@
+package EmojiMapper;
+
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+
+import org.apache.storm.Config;
+import org.apache.storm.StormSubmitter;
+import org.apache.storm.mongodb.bolt.MongoInsertBolt;
+import org.apache.storm.mongodb.common.mapper.MongoMapper;
+import org.apache.storm.mongodb.common.mapper.SimpleMongoMapper;
+import org.apache.storm.topology.TopologyBuilder;
+import org.apache.storm.tuple.Fields;
+import org.apache.storm.tuple.Tuple;
+import org.apache.storm.tuple.Values;
+
+public class MongoInsertFilteredTweetsBolt {
+
+    private static final String url = "mongodb://172.31.46.223:27017/tweetsdb";
+    //private static final String dbName = "tweetsdb";
+    private static final String collectionName = "filteredTweets";
+
+    MongoMapper mapper = new SimpleMongoMapper()
+            .withFields("tweets");
+
+    MongoInsertBolt insertBolt = new MongoInsertBolt(url, collectionName, mapper);
+
+}
