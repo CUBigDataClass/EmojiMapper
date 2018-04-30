f9e9aa44eac1a24952549d7de6188019dd0385ac
Harshini Muthukrishnan <harshinimkrishnan@gmail.com>
2018-04-30 02:00:02 -0600

    Add files via upload

--- frontend/MERN-boilerplate/server/routes/api/getData.js
+++ frontend/MERN-boilerplate/server/routes/api/getData.js
@@ -1,9 +1,8 @@
 const Date = require('../../models/GetData');
+const Emoji = require('../../models/GetEmoji');
 
 module.exports = (app) => {
   app.get('/api/test', (req, res, next) => {
-    debugger;
-    console.log(req);
     Date.find()
       .exec()
       .then((date) => res.json(date))
@@ -19,11 +18,19 @@ module.exports = (app) => {
 
   app.get('/api/test/:date/:trends', function (req, res, next) {
     var locationTrend = req.params.trends;
-    console.log(locationTrend);
-    console.log(req.params);
     Date.find({ date: req.params.date }, {[`locations.${locationTrend}`] : 1, _id: 0})
       .exec()
       .then((locations) => res.json(locations))
+      .catch((err) => next(err));
+  });
+
+  app.get('/api/emoji/:date/:trends', function (req, res, next) {
+    var locationTrend = req.params.trends;
+    console.log(req.params);
+    Emoji.aggregate([ { $match: { "date": req.params.date, "trend": req.params.trends } }, 
+                        { $group: { _id: "$emoji", total: { $sum: "$count"} } } ])
+      .exec()
+      .then((emoji) => res.json(emoji))
       .catch((err) => next(err));
   });
 }
